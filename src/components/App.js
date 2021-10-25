8ced77169e909173ec03820eef3b8652fee0183d
chronixlo <tkarvonen@gmail.com>
2021-10-25 17:21:10 +0300

    Add crude UI layout

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className="app">
+      <div className="card" style={{ backgroundColor: "#70FFD319" }}>
+        bio
+      </div>
+
+      <div className="card" style={{ backgroundColor: "#70FFD319" }}>
+        directory
+      </div>
+
+      <div className="card" style={{ backgroundColor: "#70FFD319", flex: 2 }}>
+        source
+      </div>
+    </div>
+  );
+}
+
+export default App;
